72ffcf60c15ba179ce7b7fcbfed2b3c62b61d8ed
irawandwiarno <irawandwiarnop2@gmail.com>
2023-02-28 16:31:11 +0700

    first add

---
+++ src/components/sidebar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="sidebar">
+    <div class="title">
+      <img src="aset/vector.png" alt="" />
+    </div>
+    <div class="item-link">
+      <router-link to="/" active-class="active">
+        <div class="link-container my-3 px-4">Home</div>
+      </router-link>
+    </div>
+    <div class="link-container my-3">
+      <div class="item-link">
+        <div class="dropdown">
+          <button
+            class="btn btn-darkred dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Choose Island
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Jawa</a></li>
+            <li><a class="dropdown-item" href="#">Sumatra</a></li>
+            <li><a class="dropdown-item" href="#">Sulawesi</a></li>
+            <li><a class="dropdown-item" href="#">Kalimantan</a></li>
+            <li><a class="dropdown-item" href="#">Bali</a></li>
+            <li><a class="dropdown-item" href="#">Papua</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Alata&display=swap");
+
+.sidebar {
+  padding: 10px;
+  align-items: left !important;
+  color: white;
+}
+
+.title {
+  margin-bottom: 50px;
+}
+
+.link-container {
+  font-family: "Alata", sans-serif;
+  background-color: #952114;
+  color: white;
+  padding: 10px;
+  border-radius: 8px;
+}
+.link-container:link {
+  text-decoration: none;
+}
+
+.btn-darkred {
+  background-color: #952114 !important;
+  color: white;
+}
+
+.dropdown-menu{
+    background-color: #9A2C1F;
+}
+
+.dropdown-item{
+    color: white !important;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="dashboard">
+    <sidebar />
+    <div class="content p-5">
+      <div class="search-container">
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+
+      <div class="rekom fw-bold">Rekomendasi</div>
+      <div class="row">
+        <div class="col-3">
+          <div class="container">
+            <div class="card p-1" style="width: 12rem">
+              <img src="aset/nasi-goreng.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Nasi Goreng</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="container">
+            <div class="card p-1" style="width: 12rem">
+              <img src="aset/bakso.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Bakso</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="container">
+            <div class="card p-1" style="width: 12rem">
+              <img src="aset/sate.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Sate</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div class="container">
+            <div class="card p-1" style="width: 12rem">
+              <img src="aset/rendang.png" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Rendang</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="rekom fw-bold">Popular food</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sidebar from "@/components/sidebar.vue";
+
+export default {
+  name: "Dashboard",
+  components: {
+    sidebar,
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Alata&family=Molengo&display=swap");
+
+.dashboard {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  background-color: #aa2b1d;
+  height: 100vh;
+  width: 100vw;
+}
+
+.content {
+  background-color: #efeae5;
+}
+
+.search-container {
+  width: 25rem;
+}
+
+.rekom {
+  font-family: "Alata", sans-serif;
+  color: black;
+  font-size: 30px;
+}
+
+.card-title {
+  font-family: "Molengo", sans-serif;
+  font-size: 20px;
+}
+
+.card-body {
+  padding: 0px !important;
+}
+</style>
